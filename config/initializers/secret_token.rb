3c5797207894275b4d00e6d012f0522bca3ad76b
Erik Dohnberg <erikdohnberg@gmail.com>
2013-03-19 14:19:23 -0400

    Initial commit. Set up directory for web application files.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Photogur::Application.config.secret_token = '149e9090521823f686c05ad0bd3cf0a6ac2098f4c08421bf428ac9365d321f4b2e51b93554841cd8c63cf83388677cd87306231307a560bfd5977c5604ebb0f3'
