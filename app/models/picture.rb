44c5119c29105cace551889ee4a237e50b0cbe8d
Erik Dohnberg <erikdohnberg@gmail.com>
2013-03-20 14:46:43 -0400

    Enabled mass-assignment of attributes

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,2 +1,5 @@
 class Picture < ActiveRecord::Base
+	# Very different from accr_accessor!!
+	# Allows mass-assignment for the attributes title and artist
+	attr_accessible :title, :artist, :url
 end
