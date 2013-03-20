9f44181b290b5637369934b96faf7f257697f0c0
Erik Dohnberg <erikdohnberg@gmail.com>
2013-03-20 09:32:35 -0400

    Tons of changes done during class-time again

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -8,6 +8,14 @@ class PicturesController < ApplicationController		# NOT:ActionController::Base
 		@picture = @pictures[params[:id].to_i]
 	end
 
+	def new
+	end
+
+	def create
+		render :text => "Saving a picture. Url: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
+		
+	end
+
 	def load_pictures
 		@pictures = [
 			{
@@ -16,7 +24,7 @@ class PicturesController < ApplicationController		# NOT:ActionController::Base
 			 :url =>"http://distilleryimage11.s3.amazonaws.com/20f21366904511e2bbd422000a1f9ab2_7.jpg"
 		    },
 		    {
-			 :title =>"Poo Super Class Method",
+			 :title =>"'Poo' Super Class Method",
 			 :artist =>"Hamant",
 			 :url =>"http://distilleryimage6.s3.amazonaws.com/8158dad88c5211e282e022000a1fbc68_7.jpg"
 			},
