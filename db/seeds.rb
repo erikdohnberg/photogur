5cd8a329752d165351fa2535d66a180824dbe589
Erik Dohnberg <erikdohnberg@gmail.com>
2013-03-22 08:44:57 -0400

    Added two partials and pulled from them on both new.html and edit.html

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,17 +10,17 @@
 pictures = Picture.create [
 	{
 	 :title =>"Stay classy Planet Earth",
-	 :artist =>"Irfan",
+	 :artist =>"Gavin",
 	 :url =>"http://distilleryimage11.s3.amazonaws.com/20f21366904511e2bbd422000a1f9ab2_7.jpg"
     },
     {
 	 :title =>"'Poo' Super Class Method",
-	 :artist =>"Hamant",
+	 :artist =>"Hemant",
 	 :url =>"http://distilleryimage6.s3.amazonaws.com/8158dad88c5211e282e022000a1fbc68_7.jpg"
 	},
 	{
-	 :title =>"Hamant Impersonations",
-	 :artist =>"Hamant",
+	 :title =>"Hemant Impersonations",
+	 :artist =>"Hemant",
 	 :url =>"http://distilleryimage7.s3.amazonaws.com/f8bcd6528c5111e28b6c22000a1f8db8_7.jpg"
 	}
 ]
